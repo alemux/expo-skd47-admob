8da1a9d8db92349dfbd892f6533aff5a1131a33f
Alessandro Mussini <amussini@gmail.com>
2022-11-21 17:36:55 +0100

    Update

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import mobileAds from 'react-native-google-mobile-ads';
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [adInit, setAdInit] = useState(false);
+
+  mobileAds()
+  .initialize()
+  .then(adapterStatuses => {
+    // Initialization complete!
+    // Check if any ad networks failed to initialize:
+    adapterStatuses.forEach(adapterStatus => {
+      if (adapterStatus.state === 'error') {
+        console.error(
+          'Adapter failed to initialize: ' + adapterStatus.description
+        );
+      }
+
+      setAdInit(true);
+
+    });
+    
+
+
+  });
+
+  if(!adInit) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    )
+  }else{
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
--- readme.md
+++ readme.md
@@ -5,3 +5,11 @@ https://docs.page/invertase/react-native-google-mobile-ads
 La vecchia libreria, perfettamente funzionante su <= SDK45 con i nuovi SDK non funziona più.
 In più ho la problematica di non riuscire ad installare i podfile su processore M1Pro.
 Quindi per ora manterrò lo sviluppo su processore Intel, cercando di risolvere i problemi di installazione iniziali.
+
+# Non utilizziamo più la build di expo, useremo EAS BUILD, che esclude il file .env
+Risoluzione: duplicare il file .gitignore, rinominarlo .easignore rimuovendo il file .env tra le esclusioni
+
+# Dispositivo di test
+Ovviamente facendo riferimento a Native Modules, l'app di ExpoGo diventa inutilizzabile: procedo con la creazione di build di dev come da istruzioni.
+=> npx eas-cli build --profile development
+
